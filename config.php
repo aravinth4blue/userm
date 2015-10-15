c373b46e7c617ec5d2dc2e507af3ed87eef6de2b
Aravinth <aravinth.krishnamoorthi@gmail.com>
2015-10-15 21:50:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'adminfIIlvK7');
-define('DB_PASSWORD', 'amBY3_pPtD-K');
-define('DB_DATABASE', 'reg');
+define('DB_USERNAME', 'admint56uKyw');
+define('DB_PASSWORD', 'B8Upacq3JlED');
+define('DB_DATABASE', 'php');
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD)or die("Cannot connect");
 $db=mysqli_select_db($conn,DB_DATABASE) or die("Failed to connect to MySQL: " . mysqli_error($conn));
 ?>
