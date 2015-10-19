e5b4e156864a49982514bc21880fb3aa27d0a632
aravinth4blue <aravinth.krishnamoorthi@gmail.com>
2015-10-19 12:14:31 +0530

    commited

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'openshift.redhat.com');
+define('DB_USERNAME', 'admint56uKyw');
+define('DB_PASSWORD', ' B8Upacq3JlED');
 define('DB_DATABASE', 'reg');
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD)or die("Cannot connect");
 $db=mysqli_select_db($conn,DB_DATABASE) or die("Failed to connect to MySQL: " . mysqli_error($conn));
