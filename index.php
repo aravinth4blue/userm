95e60c62925acd3a65d50c3a7e0d456525d41c10
aravinth4blue <aravinth.krishnamoorthi@gmail.com>
2015-10-20 18:07:12 +0530

    commited

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include 'header.php'; ?>        
-
+<?php echo hi; ?>
 <div class="jumbotron vertical-center"> 
         	<div class="container ">
 					<h2 style="text-align: center;">Registration/Login system using bootstrap</h2>
