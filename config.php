fdf83bd2e962a3809659bf31e60239ec28f6b95b
aravinth4blue <aravinth.krishnamoorthi@gmail.com>
2015-10-21 07:12:13 +0530

    commited

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 define('DB_SERVER', getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT'));
+var_dump(DB_SERVER);
+die;
 define('DB_USERNAME', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_DATABASE', getenv('OPENSHIFT_APP_NAME'));
