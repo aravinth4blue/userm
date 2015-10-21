c4ae3aeb796994c38e3b1c4fd7deef6ba88c4e4c
aravinth4blue <aravinth.krishnamoorthi@gmail.com>
2015-10-21 21:23:32 +0530

    commited

--- config.php
+++ config.php
@@ -4,12 +4,9 @@ define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USERNAME', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_DATABASE', getenv('OPENSHIFT_APP_NAME'));
-echo $OPENSHIFT_MYSQL_DB_HOST;
-var_dump(getenv('OPENSHIFT_MYSQL_DB_HOST'));
-var_dump(getenv('OPENSHIFT_MYSQL_DB_PORT'));
-var_dump(DB_DATABASE);
+
 
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,"",DB_PORT)or die("Cannot connect" .mysqli_error($conn));
-var_dump($conn);
+
 $db=mysqli_select_db($conn,DB_DATABASE) or die("Failed to connect to MySQL: " . mysqli_error($conn));
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
 <?php include 'header.php'; ?>        
-<?php echo hi; ?>
+
 <div class="jumbotron vertical-center"> 
         	<div class="container ">
 					<h2 style="text-align: center;">Registration/Login system using bootstrap</h2>
