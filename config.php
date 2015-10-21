1696bce1aecb507963f6db0244acdfdd8ff94b4c
aravinth4blue <aravinth.krishnamoorthi@gmail.com>
2015-10-21 08:20:39 +0530

    commited

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USERNAME', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_DATABASE', getenv('OPENSHIFT_APP_NAME'));
-var_dump($OPENSHIFT_MYSQL_DB_HOST);
-var_dump(DB_SERVER);
-var_dump(DB_PORT);
+
+var_dump(getenv('OPENSHIFT_MYSQL_DB_HOST'));
+var_dump(getenv('OPENSHIFT_MYSQL_DB_PORT'));
 var_dump(DB_DATABASE);
 
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,"",DB_PORT)or die("Cannot connect" .mysqli_error($conn));
